df3e14dfb336ffa3a4bc1df24c4d6042c88f7e32
Fadzli Razali <fadzlitown@gmail.com>
2015-01-28 03:01:53 +0800

    Added the event interaction on app

--- app/src/main/java/com/fadzlirazali/wocoapp/DrawingCanvas.java
+++ app/src/main/java/com/fadzlirazali/wocoapp/DrawingCanvas.java
@@ -1,8 +1,17 @@
 package com.fadzlirazali.wocoapp;
 
+import android.graphics.Color;
 import android.view.View;
 import android.content.Context;
 import android.util.AttributeSet;
+
+/*import graphic library*/
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+
 
 /**
  * 1. Setup the Drawing class properties and methods from implementation
@@ -10,6 +19,18 @@ import android.util.AttributeSet;
  * Created by FadzliRazali on 1/27/15.
  */
 public class DrawingCanvas extends View {
+
+    //instance variables
+    //to trace user's drawing moving finger action on the canvas
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //init color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
 
 
     //Constructor with args
@@ -24,10 +45,103 @@ public class DrawingCanvas extends View {
 
     //add an instance of the custom View to the XML layout file
     private void setupDrawing() {
-        //get drawing area up
 
+        //obj variables to set the class up for drawing
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        //set the init color
+        drawPaint.setColor(paintColor);
+
+        //set the init path properties
+        drawPaint.setAntiAlias(true);   //smooths out the edges of what is being drawn
+        drawPaint.setStrokeWidth(10);   //stroke thickness
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        /*Instantiating the canvas Paint object*/
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
 
     }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        //call the superclass
+        super.onSizeChanged(w,h,oldw,oldh);
+
+        /*
+        * ARGB_8888
+        * Each channel (RGB and alpha
+        * for translucency) is stored with 8 bits of precision (256 possible values.)
+        * */
+        //instantiate the drawing canvas and bitmap
+        //Returns a mutable bitmap with the specified width and height
+        canvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        //allow to draw from custom canvas
+        canvas.drawBitmap(canvasBitmap,0,0,canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+
+    }
+
+    /*
+    * 2.Event Drawing :   detect user touch
+    * */
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        //implement drawing are down, move and up in switch statement
+        switch (event.getAction()){
+
+            //jari user sentuh ke screen, akan mula dilukis(drawPath): move to that position to start drawing
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX,touchY);
+                break;
+
+            //jari user sentuh gerak di sekitar screen : draw the path along with their touch
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX,touchY);
+                break;
+
+            //jari user lift up on the screen : draw the Path and reset it for the next drawing operation
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+
+            default:
+                return false;
+        }
+
+        //invalidate: cause the onDraw method to execute
+        invalidate();
+        return true;
+    }
+
+
+    /*
+    * 3.Choose Color
+    * */
+
+    /*set the color method*/
+    public void setColor(String newColor){
+        //set color
+
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+
+
 
 
 }
--- app/src/main/java/com/fadzlirazali/wocoapp/MainActivity.java
+++ app/src/main/java/com/fadzlirazali/wocoapp/MainActivity.java
@@ -1,18 +1,66 @@
 package com.fadzlirazali.wocoapp;
 
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/*3.Choosing Colors: ability for the user to choose colors from the palette*/
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
+
+    //instance variable: represents the instance of the DrawingCanvas class View
+    private DrawingCanvas drawCanvas;
+
+    //instance variable to represent the paint color button
+    private ImageButton currentPaint;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* akan invoked DrawingCanvas view by ID di activity_main.xml : View that is displayed in the Activity*/
+        drawCanvas=(DrawingCanvas)findViewById(R.id.drawing);
+
+        /*invoked to ColorPallete from id paint_colors : retrieve the first paint color, which is to be selected*/
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+
+        /*Dapatkan first btn dan simpan di instance variable*/
+        currentPaint=(ImageButton)paintLayout.getChildAt(0);
+
+        /*drawable area img is selected*/
+        currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
+
+    /*
+    * let the user choose colors
+    * */
+    public void paintCLicked(View view){
+        //user pick the color
+
+        /*check that the user has clicked a paint color*/
+        if(view != currentPaint){
+            //update color
+
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();    //get the color palate tag values
+
+            /*call the setColor method on DrawingCanvas View obj*/
+            drawCanvas.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currentPaint=(ImageButton)view;
+        }
+    }
+
+
 
 
     @Override
